da9e5f53140aacc2e906eb7c939bbc8990307591
Anatomyah <anatomyah@protonmail.com>
2023-11-21 12:28:04 +0200

    21.11.23

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -12,7 +12,7 @@ import UpdateAmountModal from "../../components/Product/UpdateAmountModal";
 import NotificationCard from "../../components/Generic/NotificationCard";
 
 const HomePage = () => {
-  const { notifications } = useContext(AppContext);
+  const { notifications, isSupplier } = useContext(AppContext);
   const [showProductModal, setShowProductModal] = useState(false);
   const [showOrderModal, setShowOrderModal] = useState(false);
   const [showQuoteModal, setShowQuoteModal] = useState(false);
@@ -47,45 +47,51 @@ const HomePage = () => {
             </h1>
           </Col>
           <Col sm={4} className="text-center">
-            <h1>
-              <Badge pill bg="secondary">
-                Order Notifications
-              </Badge>
-            </h1>
+            {!isSupplier && (
+              <h1>
+                <Badge pill bg="secondary">
+                  Order Notifications
+                </Badge>
+              </h1>
+            )}
           </Col>
         </Row>
         <Row>
           <Col sm={8} className="d-flex flex-row flex-wrap">
-            <div className="col-md-5 mx-auto home-box-style">
-              <Button
-                variant="secondary"
-                className="w-100 h-100"
-                style={{ fontSize: "30px", fontWeight: "bold" }}
-                onClick={handleCreateOrder}
-              >
-                Receive Order
-              </Button>
-            </div>
-            <div className="col-md-5 mx-auto home-box-style">
-              <Button
-                variant="secondary"
-                className="w-100 h-100"
-                style={{ fontSize: "30px", fontWeight: "bold" }}
-                onClick={handleUpdateStock}
-              >
-                Update Stock
-              </Button>
-            </div>
-            <div className="col-md-5 mx-auto home-box-style">
-              <Button
-                variant="secondary"
-                className="w-100 h-100"
-                style={{ fontSize: "30px", fontWeight: "bold" }}
-                onClick={handleCreateQuote}
-              >
-                Create Quote
-              </Button>
-            </div>
+            {!isSupplier && (
+              <>
+                <div className="col-md-5 mx-auto home-box-style">
+                  <Button
+                    variant="secondary"
+                    className="w-100 h-100"
+                    style={{ fontSize: "30px", fontWeight: "bold" }}
+                    onClick={handleCreateOrder}
+                  >
+                    Receive Order
+                  </Button>
+                </div>
+                <div className="col-md-5 mx-auto home-box-style">
+                  <Button
+                    variant="secondary"
+                    className="w-100 h-100"
+                    style={{ fontSize: "30px", fontWeight: "bold" }}
+                    onClick={handleUpdateStock}
+                  >
+                    Update Stock
+                  </Button>
+                </div>
+                <div className="col-md-5 mx-auto home-box-style">
+                  <Button
+                    variant="secondary"
+                    className="w-100 h-100"
+                    style={{ fontSize: "30px", fontWeight: "bold" }}
+                    onClick={handleCreateQuote}
+                  >
+                    Create Quote
+                  </Button>
+                </div>
+              </>
+            )}
             <div className="col-md-5 mx-auto home-box-style">
               <Button
                 variant="secondary"
@@ -98,22 +104,24 @@ const HomePage = () => {
             </div>
           </Col>
           <Col sm={4}>
-            <div className="scrollable-area">
-              {notifications &&
-                notifications.map((notification) => (
-                  <NotificationCard
-                    key={notification.pk}
-                    props={{
-                      catNum: notification.fields.product_cat_num,
-                      productName: notification.fields.product_name,
-                      supplierName: notification.fields.supplier_name,
-                      currentStock: notification.fields.current_stock,
-                      lastOrdered: notification.fields.last_ordered,
-                      avgOrderTime: notification.fields.avg_order_time,
-                    }}
-                  />
-                ))}
-            </div>
+            {!isSupplier && (
+              <div className="scrollable-area">
+                {notifications &&
+                  notifications.map((notification) => (
+                    <NotificationCard
+                      key={notification.pk}
+                      props={{
+                        catNum: notification.fields.product_cat_num,
+                        productName: notification.fields.product_name,
+                        supplierName: notification.fields.supplier_name,
+                        currentStock: notification.fields.current_stock,
+                        lastOrdered: notification.fields.last_ordered,
+                        avgOrderTime: notification.fields.avg_order_time,
+                      }}
+                    />
+                  ))}
+              </div>
+            )}
           </Col>
         </Row>
       </Container>
