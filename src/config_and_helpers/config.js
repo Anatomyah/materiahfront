24f7afbcf5ab019cf4fb8d81e933e737b31fb1a0
Anatomyah <anatomyah@protonmail.com>
2023-12-07 13:55:11 +0200

    07.12.23

--- src/config_and_helpers/config.js
+++ src/config_and_helpers/config.js
@@ -1,7 +1,12 @@
 // export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
+// DEMO EC2
 export const BACKEND_URL =
-  "http://ec2-3-70-176-24.eu-central-1.compute.amazonaws.com/v1/";
+  "http://ec2-52-28-42-227.eu-central-1.compute.amazonaws.com";
+
+// ORGANOIDS EC2
+// export const BACKEND_URL =
+//   "http://ec2-3-70-176-24.eu-central-1.compute.amazonaws.com/v1/";
 
 export const defaultImageUrl =
   "https://materiah1.s3.eu-central-1.amazonaws.com/products/No-Image-Placeholder.svg.png";
