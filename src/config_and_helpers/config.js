f116a61544c5be283afd3fd7401d0a73fad40f79
Anatomyah <anatomyah@protonmail.com>
2024-04-17 17:24:21 +0300

    Added location field for the product model

--- src/config_and_helpers/config.js
+++ src/config_and_helpers/config.js
@@ -73,6 +73,7 @@ export const PRODUCT_CATEGORIES = [
 export const PRODUCT_MEASUREMENT_UNITS = [
   { value: "L", label: "Litres, l" },
   { value: "ML", label: "Milliliters, ml" },
+  { value: "UL", label: "Milliliters, µl" },
   { value: "KG", label: "Kilograms,kg" },
   { value: "G", label: "Grams, g" },
   { value: "MG", label: "Milligrams, mg" },
