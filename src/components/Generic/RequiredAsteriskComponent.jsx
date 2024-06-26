fe50d8641202067ce55eed1368d230c5fb0592bf
Anatomyah <anatomyah@protonmail.com>
2024-06-26 16:40:22 +0300

    Added location field for the product model

---
+++ src/components/Generic/RequiredAsteriskComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./GenericComponentsStyle.css";
+
+const RequiredAsteriskComponent = () => {
+  return <span className="required-asterisk">*</span>;
+};
+export default RequiredAsteriskComponent;
